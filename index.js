fe3430bde44636f60a37aff294c1908d6ede1703
Sam <smiths37@miamioh.edu>
2022-06-08 19:25:24 -0400

    Added text in java

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 let header = document.querySelector('#page-header')
 header.style.textAlign = "left"
-...
-let dogImages = document.querySelectorAll('.dog-image')
-for(let i = 0; i < dogImages.length; i++){
-	dogImages[i].style.borderRadius = '50px'
-}
+docyment.getElementByID("Doggos").innerHTML="The Best Woofers";
 
 // Select some elements...
